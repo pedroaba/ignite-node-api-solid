00c79716765ca5bf492e0272363ac0e59f7de234
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2023-12-16 21:50:47 -0300

    feat: tests e2e for gyms controllers

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,57 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Nearby Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to search nearby gym", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "JavaScript Gym",
+        description: "Some description.",
+        phone: "",
+        latitude: -22.2451207,
+        longitude: -45.7045176,
+      });
+
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "TypeScript Gym",
+        description: "Some description.",
+        phone: "",
+        latitude: 1.1041397,
+        longitude: -66.268836,
+      });
+
+    const response = await request(app.server)
+      .get("/gyms/nearby")
+      .query({
+        latitude: -22.2451207,
+        longitude: -45.7045176,
+      })
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.gyms).toHaveLength(1);
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: "JavaScript Gym",
+      }),
+    ]);
+  });
+});
